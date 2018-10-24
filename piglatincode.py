34c55b802d4ec20f1c9691a1f8a30200b3c9c1b2
amk6451 <amk6451@psu.edu>
2018-10-24 13:00:24 -0400

    dafv

---
+++ piglatincode.py
@@ -0,0 +1,16 @@
+def pig_latin(text):
+    for words in text.split():
+        if words[0] in "aeiou":
+                return words + 'yay'
+        else:
+            counter = 0
+            counter2 = 0
+            while counter2 == 0:
+                if words[counter] not in "aeiouy":
+                    counter += 1
+                else:
+                    counter2 = 1
+            return words[counter:] + words[:counter] + 'ay'
+
+text = input("Please enter text:")
+print(' '.join(pig_latin(words) for words in text.split()))
